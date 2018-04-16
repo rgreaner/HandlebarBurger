ef60931e0745d9fd24c1e3df8d869f9dad82216c
Rachel Greaner <rgreaner8@gmail.com>
2018-04-16 17:34:29 -0500

    Final (?) Commit

--- config/orm.js
+++ config/orm.js
@@ -1,30 +1,6 @@
 // Import (require) connection.js into orm.js
 var connection = require("../config/connection.js");
 
-// In the orm.js file, create the methods that will execute the necessary MySQL commands in the controllers. 
-
-// Helper function to convert object key/value pairs to SQL syntax
-function objToSql(ob) {
-    var arr = [];
-
-    // loop through the keys and push the key/value as a string int arr
-    for (var key in ob) {
-        var value = ob[key];
-        // check to skip hidden properties
-        if (Object.hasOwnProperty.call(ob, key)) {
-            // if string with spaces, add quotations (California Burger => 'California Burger')
-            if (typeof value === "string" && value.indexOf(" ") >= 0) {
-                value = "'" + value + "'";
-            }
-            // e.g. {name: 'California Burger'} => ["name='California Burger'"]
-            // e.g. {Devoured: true} => ["Devoured=true"]
-            arr.push(key + "=" + value);
-        }
-    }
-    // translate array of strings to a single comma-separated string
-    return arr.toString();
-}
-
 //These are the methods you will need to use in order to retrieve and store data in your database.
 // selectAll()
 // insertOne()
